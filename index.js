379c9c15f6f0c350cf403ef01f224c607ee33085
Pedro Lorenzo Rosario <63958751+PALR-1216@users.noreply.github.com>
2022-07-21 04:10:12 +0000

    i need to fix the page and current page api

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ const client = new webtorrent();
 
 app.set('views', path.join('views'));
 app.set('view engine', 'ejs');
-let movieData = [];
 
-let url = `https://yts.mx/api/v2/list_movies.json?quality=1080p&page=1&limit=20&query_term=`
+
+
 app.get('/',async(req,res) =>{
+    let movieData = [];
+    let url = `https://yts.mx/api/v2/list_movies.json?quality=1080p&page=${1}&limit=20&query_term=`
     await axios.get(url).then((allData) =>{
         let data = allData.data.data.movies
-        let page = allData.data.page_number;
+        let page = allData.data.data.page_number;
         for(i in data) {
             
             let name = data[i].title;
@@ -36,11 +38,57 @@ app.get('/',async(req,res) =>{
                 
             }
             movieData.push(movieObject)
+            
         }
-        res.render('home', {model:movieData, pageNumber:page})
+        
+        res.render('home', {model:movieData, page:page })
         // res.json(movieData)
     })
     // res.render('home')
 })
 
+app.get('/nextPage/:pageNumber', async(req,res)=>{
+    let movieData = [];
+    let currentPage = req.params.pageNumber;
+    let urlNext = `https://yts.mx/api/v2/list_movies.json?quality=1080p&page=${currentPage + 1}&limit=20&query_term=`
+    await axios.get(urlNext).then((allData) =>{
+        let data = allData.data.data.movies;
+        let page = currentPage;
+
+        for(i in data) {
+            
+            let name = data[i].title;
+            let imdb_code = data[i].imdb_code;
+            let year = data[i].year;
+            let large_cover_image = data[i].large_cover_image;
+            let small_cover_image = data[i].small_cover_image;
+            let torrents = data[i].torrents;
+            let slug = data[i].slug;
+            let movieObject = {
+                page_number:currentPage,
+                name:name,
+                imdb_code:imdb_code,
+                year:year,
+                small_cover_image, small_cover_image,
+                large_cover_image:large_cover_image,
+                slug:slug,
+                torrents:torrents[1],
+            }
+            movieData.push(movieObject)
+        
+        }
+        console.log(currentPage)
+
+        res.render('home', {model:movieData, page:page })
+    })
+
+})
+
+
+
 app.listen(3000)
+
+
+async function insertData(url, name, year, imdb_code, smallImage, largeImage, slug, page, torrent) {
+
+}
