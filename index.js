4ee70e7fa87448b65bd5067daa178c034202150b
Pedro Lorenzo Rosario <63958751+PALR-1216@users.noreply.github.com>
2022-07-21 11:47:25 +0000

    dfgfg

--- index.js
+++ index.js
@@ -51,50 +51,34 @@ app.get('/nextPage/:pageNumber', async(req,res)=>{
     let movieData = [];
     let currentPage = req.params.pageNumber;
     let urlNext = `https://yts.mx/api/v2/list_movies.json?quality=1080p&page=${currentPage + 1}&limit=20&query_term=`
-    await axios.get(urlNext).then((allData) =>{
-        let data = allData.data.data.movies;
-        let page = currentPage;
-
-        for(i in data) {
-            
-            let name = data[i].title;
-            let imdb_code = data[i].imdb_code;
-            let year = data[i].year;
-            let large_cover_image = data[i].large_cover_image;
-            let small_cover_image = data[i].small_cover_image;
-            let torrents = data[i].torrents;
-            let slug = data[i].slug;
-            let movieObject = {
-                page_number:currentPage,
-                name:name,
-                imdb_code:imdb_code,
-                year:year,
-                small_cover_image, small_cover_image,
-                large_cover_image:large_cover_image,
-                slug:slug,
-                torrents:torrents[1],
-            }
-            movieData.push(movieObject)
-        
-        }
-        console.log(currentPage)
-
-        res.render('home', {model:movieData, page:page })
-    })
+    
 
 })
 
 app.get('/watch/:slug/:hash/:title', async(req,res) =>{
     let magnet = `magnet:?xt=urn:btih:${req.params.hash}&dn=${req.params.slug}&tr=http://track.one:1234/announce&tr=udp://p4p.arenabg.com:1337&udp://tracker.leechers-paradise.org:6969&udp://tracker.openbittorrent.com:80`
     res.render('video', {videoMagnet: magnet, movieName:req.params.title})
+    getTorrentData(req.params.hash, req.params.slug);
 
 })
+
+
+function getTorrentData(slug, hash) {
+    //TODO:create a webtorrent api to stream the movie
+    let torrentId = `magnet:?xt=urn:btih:${hash}&dn=${slug}&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent`
+    client.add(torrentId, (torrent) =>{
+        const file = torrent.files.find((file) =>{
+            return file.name.endsWith('.mp4')
+        })
+        console.log(file)
+    })
+}
+
+
+
 
 
 
 app.listen(3000)
 
 
-async function insertData(url, name, year, imdb_code, smallImage, largeImage, slug, page, torrent) {
-
-}
